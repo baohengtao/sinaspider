6a661d3ccf70487387afd0bfbd81d4756bce8f66
Bao Hengtao <baohengtao@users.noreply.github.com>
2023-03-14 04:34:49 +0800

    add clean database command to script

--- sinaspider/script/database.py
+++ sinaspider/script/database.py
@@ -130,3 +130,30 @@ def _get_update():
             weibo.save()
         else:
             yield weibo
+
+
+@app.command()
+def database_clean():
+    import questionary
+    from photosinfo.model import Photo
+
+    from sinaspider.model import Artist, User, UserConfig, Weibo
+    if not questionary.confirm('Have you backup database to rpi?').ask():
+        console.log('Backup first, bye!')
+        return
+    if not questionary.confirm('Have you put all photos to Photos.app?').ask():
+        console.log('put them to Photos.app first, bye!')
+        return
+    photos = (Photo.select()
+              .where(Photo.image_supplier_name == "Weibo")
+              .where(Photo.image_unique_id.is_null(False)))
+    photo_bids = {p.image_unique_id for p in photos}
+    console.log(f'{len(photo_bids)} weibos in photos.app\n'
+                f'{len(Weibo)} weibos in sina database')
+    del_count = Weibo.delete().where(Weibo.bid.not_in(photo_bids)).execute()
+    console.log(f'{del_count} weibos have been deleted\n'
+                f'{len(Weibo)} weibos left in sina database')
+    uids = {u.user_id for u in UserConfig} | {u.user_id for u in Artist}
+    del_count = User.delete().where(User.id.not_in(uids)).execute()
+    console.log(f'{del_count} users have been deleted')
+    console.log('Done!')
